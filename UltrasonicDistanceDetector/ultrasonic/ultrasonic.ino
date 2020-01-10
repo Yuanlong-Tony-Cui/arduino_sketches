d328d1998023eef7c4236b5f6748d6d1f0315d43
Yuanlong-Tony-Cui <yuanlongcui0511@gmail.com>
2020-01-09 23:21:25 -0500

    Added Passive Buzzer and made LED dim instead of blink

--- UltrasonicDistanceDetector/ultrasonic/ultrasonic.ino
+++ UltrasonicDistanceDetector/ultrasonic/ultrasonic.ino
@@ -8,15 +8,22 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int trigPin = 8, echoPin = 9;
 const int ledPin = 13;
-
+const int buzzerPin = 6;
 long duration;
 int distCm;
+int brightness;
+int frequency;
 
-void ledBlink(int numMs) {
-  digitalWrite(ledPin, HIGH);
-  delay(numMs*10);
-  digitalWrite(ledPin, LOW);
-  delay(numMs*10);
+int getDist(long period) {
+  return period * 0.034 / 2;
+}
+
+void displayDist(int dist) {
+  lcd.setCursor(0, 1); // Starts on the 2nd line.
+  lcd.print("Distance: ");
+  lcd.print(distCm);
+  lcd.print(" cm");
+  lcd.print("                "); // Erases all remaining chars.
 }
 
 void setup() {
@@ -29,19 +36,27 @@ void setup() {
 
 void loop() {
   digitalWrite(trigPin, HIGH);
-  delay(1);
+  delayMicroseconds(100);
   digitalWrite(trigPin, LOW);
-
-  duration = pulseIn(echoPin, HIGH);
-  distCm = duration * 0.034 / 2;
-
-  lcd.setCursor(0, 1); // Starts on the 2nd line.
-  lcd.print("Distance: ");
-  lcd.print(distCm);
-  lcd.print(" cm");
-  lcd.print("                "); // Erases all remaining chars.
-
-  ledBlink(distCm);
   
-  // delay(500); // Checks every 500 ms.
+  duration = pulseIn(echoPin, HIGH);
+  distCm = getDist(duration);
+
+  // Handle out-of-range scenarios:
+  if (distCm > 400) {
+    distCm = 400;
+  }
+  if (distCm < 2) {
+    distCm = 2;
+  }
+
+  displayDist(distCm);
+
+  brightness = map(distCm, 400, 2, 0, 255);
+  analogWrite(ledPin, brightness);
+
+  frequency = map(distCm, 400, 2, 100, 1000);
+  tone(buzzerPin, frequency);
+  
+  delay(100); // Checks every 100 ms.
 }
